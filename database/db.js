5a7abb9220c81528f93be985a74b00c86b639d6a
Mesfin Tsegaye <sciemesfin55@gmail.com>
2020-09-04 11:35:05 +0300

    express node js and mysql auth app

---
+++ database/db.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+const db = {};
+const sequelize = new Sequelize("autlify", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  operatorsAliases: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+module.exports = db;
---
+++ models/User.js
@@ -0,0 +1,34 @@
+const Sequelize = require("sequelize");
+const db = require("../database/db");
+module.exports = db.sequelize.define(
+  "user",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: Sequelize.STRING,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+    },
+    middleName: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
+    created: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
